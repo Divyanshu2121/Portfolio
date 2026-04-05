b49d51d17b19c2859bf873d5fa2a769468a575b4
Divyanshu Sharma <sharmadivyanshu015@gmail.com>
2026-04-05 08:27:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-window.addEventListener("scroll", function(){
-    document.querySelectorAll("section").forEach(section=>{
-        const position = section.getBoundingClientRect().top;
-        const screen = window.innerHeight;
-        if(position < screen - 100){
-            section.style.opacity = 1;
-            section.style.transform = "translateY(0)";
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        const top = sec.getBoundingClientRect().top;
+        const trigger = window.innerHeight - 100;
+
+        if(top < trigger){
+            sec.classList.add("show");
         }
     });
 });
